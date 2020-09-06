8718b2ff4c95e182f1435c028be8601eda1d4c4c
Jorge Miranda <j.miranda@druo.com>
2020-09-06 17:43:00 -0500

    MirandaMall Application Initialized load of assets images for mocks and icon Landing page configured with search bar and collectionView with mock cells collectionViewConfigured for resize at change in orientation

---
+++ MirandaMall/CategoryModels.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryModels.swift
+//  MirandaMall
+//
+//  Created by Jorge Miranda on 6/09/20.
+//  Copyright © 2020 Jorge Miranda. All rights reserved.
+//
+
+import UIKit
+
+struct categoryInfo {
+    var Title: String
+    var image: UIImage
+}
+
