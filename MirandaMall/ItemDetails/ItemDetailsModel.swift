54666f1871156f2ca0fe51a69f2b640985cdf427
Jorge Miranda <jorgemiranda.co@gmail.com>
2020-09-09 23:21:03 -0500

    Init of ItemDetails ViewController and Model

---
+++ MirandaMall/ItemDetails/ItemDetailsModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemDetailsModel.swift
+//  MirandaMall
+//
+//  Created by Jorge Miranda on 9/09/20.
+//  Copyright © 2020 Jorge Miranda. All rights reserved.
+//
+
+import Foundation
+
+class ItemDetailsModel {
+    
+}
--- MirandaMall/ItemList/ItemListViewController.swift
+++ MirandaMall/ItemList/ItemListViewController.swift
@@ -46,16 +46,14 @@ class ItemListViewController: UIViewController, ItemListDelegate {
         notificationSetup()
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? ItemDetailsViewController {
+            
+        }
     }
-    */
     
     func getItems(newPage: Bool = false) {
         loadMoreItems = true
@@ -226,6 +224,10 @@ extension ItemListViewController: UITableViewDataSource, UITableViewDelegate {
         
         cell.itemInfo = info
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "showDetails", sender: ["info": self.model.itemList[indexPath.row]])
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
